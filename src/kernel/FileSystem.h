479b9f87fe3e0383d6c75c9b347e9ef76cc1bf60
Frantisek Kolenak <japan.g6@gmail.com>
2016-11-13 17:47:06 +0100

    Basic structures for Filesystem

---
+++ src/kernel/FileSystem.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <string>
+
+struct Node
+{
+	std::string name;
+	Node* parent;
+};
+
+struct File : Node
+{
+	std::string data;
+};
+
+struct Directory : Node
+{
+	Node* childs[];
+};
